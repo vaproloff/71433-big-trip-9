0c4843b3d26c724893557c6f96ae42dc0ac87845
Konstantin Pivnov <vaproloff@ya.ru>
2019-08-09 17:54:37 +0300

    Переделал отрисовку на append-ах вместо insertAdjacentHTML, добавил DocumentFragment

--- src/main.js
+++ src/main.js
@@ -6,10 +6,27 @@ import {returnEventListHtml} from "./components/event-list";
 import {returnEventHtml} from "./components/event";
 import {returnEventEditHtml} from "./components/event-edit";
 
+// Функция рендеринга компонентов
 const renderHtmlInContainer = (container, place, html) => {
-  container.insertAdjacentHTML(place, html);
+  const node = document.createElement(`div`);
+  node.innerHTML = html;
+  switch (place) {
+    case `afterbegin`:
+      container.prepend(node.children[0]);
+      break;
+    case `afterend`:
+      container.after(node.children[0]);
+      break;
+    case `beforebegin`:
+      container.before(node.children[0]);
+      break;
+    case `beforeend`:
+      container.append(node.children[0]);
+      break;
+  }
 };
 
+// Рендеринг компонентов шапки
 const tripInfoSection = document.querySelector(`section.trip-main__trip-info`);
 renderHtmlInContainer(tripInfoSection, `afterbegin`, returnTripInfoHtml());
 
@@ -17,12 +34,15 @@ const tripControlsHeadings = document.querySelectorAll(`.trip-main__trip-control
 renderHtmlInContainer(tripControlsHeadings[0], `afterend`, returnTripControlsHtml());
 renderHtmlInContainer(tripControlsHeadings[1], `afterend`, returnTripFiltersHtml());
 
-const tripEventsSection = document.querySelector(`section.trip-events`);
-renderHtmlInContainer(tripEventsSection, `beforeend`, returnTripSortHtml());
-renderHtmlInContainer(tripEventsSection, `beforeend`, returnEventListHtml());
-
-const tripEventsList = tripEventsSection.querySelector(` ul.trip-events__list`);
+// Рендеринг компонентов контента
+const mainContentFragment = document.createDocumentFragment();
+renderHtmlInContainer(mainContentFragment, `beforeend`, returnTripSortHtml());
+renderHtmlInContainer(mainContentFragment, `beforeend`, returnEventListHtml());
+const tripEventsList = mainContentFragment.querySelector(` ul.trip-events__list`);
 renderHtmlInContainer(tripEventsList, `beforeend`, returnEventEditHtml());
 renderHtmlInContainer(tripEventsList, `beforeend`, returnEventHtml());
 renderHtmlInContainer(tripEventsList, `beforeend`, returnEventHtml());
 renderHtmlInContainer(tripEventsList, `beforeend`, returnEventHtml());
+
+const tripEventsSection = document.querySelector(`section.trip-events`);
+tripEventsSection.append(mainContentFragment);

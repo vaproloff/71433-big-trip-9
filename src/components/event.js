50c2f259d54cfdc9d57f50a886523e3a4ef66e05
Konstantin Pivnov <vaproloff@ya.ru>
2019-08-15 14:07:17 +0300

    Исправлена ошибка, препятствующая загрузке картинок

--- src/components/event.js
+++ src/components/event.js
@@ -12,7 +12,7 @@ export const returnEventHtml = ({type, city, timeStart, duration, price, offers}
   <li class="trip-events__item">
     <div class="event">
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${(type === `Check-in` || type === `Restaurant` || type === `Sightseeing`) ? `in` : `to`} ${city}</h3>
 

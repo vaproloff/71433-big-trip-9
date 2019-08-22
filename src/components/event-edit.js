46d94e381049c6fa8dfd3718347e34baa20dd85e
Konstantin Pivnov <vaproloff@ya.ru>
2019-08-22 19:04:33 +0300

    Исправил ошибку

--- src/components/event-edit.js
+++ src/components/event-edit.js
@@ -113,7 +113,7 @@ class EventEditCard {
     
           <section class="event__details">
     
-            <section class="event__section  event__section--offers ${[...this._offers].length ? `` : `visually-hidden`}">
+            <section class="event__section  event__section--offers ${this._offers.size ? `` : `visually-hidden`}">
               <h3 class="event__section-title  event__section-title--offers">Offers</h3>
     
               <div class="event__available-offers">
